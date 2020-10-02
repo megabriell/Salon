e2c5a6f1a83995cb8e2f899aefa8ea27ecdec047
Manuel Gabriel <ingmanuelgabriel@gmail.com>
2020-10-02 03:26:57 -0600

    Update home.php

--- views/home/home.php
+++ views/home/home.php
@@ -1,1 +1,15 @@
 <?php include_once dirname(__file__,3)."/config/session.php"; ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box box-default">
+            <div class="box-header with-border">
+                
+            </div>
+            <div class="box-body" id="box_body">
+                <
+            </div>
+        </div>
+    </div>
+</div>
+<div id="subContent"></div>
