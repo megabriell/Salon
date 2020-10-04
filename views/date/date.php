5bfad6d6770964237640720d468e560b11241a51
Manuel Gabriel <ingmanuelgabriel@gmail.com>
2020-10-04 02:12:38 -0600

    Update date.php

--- views/date/date.php
+++ views/date/date.php
@@ -1,8 +1,3 @@
-<link rel="stylesheet" href="./plugins/calendar/css/calendar.css">
-<script src="./plugins/calendar/js/calendar.js"></script>
-<script type="text/javascript" src="./plugins/calendar/js/es-ES.js"></script>
-<script src="./plugins/underscore/underscore-min.js"></script>
-
 <div class="row">
     <div class="col-md-12">
         <div class="box box-default">
@@ -12,15 +7,15 @@
                     <div class="page-header"><h2></h2></div>
                     <div class="pull-left form-inline"><br>
                         <div class="btn-group">
-                            <button class="btn btn-default" data-calendar-nav="prev"><< Anterior</button>
-                            <button class="btn btn-default" data-calendar-nav="today">Hoy</button>
-                            <button class="btn btn-default" data-calendar-nav="next">Siguiente >></button>
+                            <button class="btn" data-calendar-nav="prev"><< Anterior</button>
+                            <button class="btn " data-calendar-nav="today">Hoy</button>
+                            <button class="btn " data-calendar-nav="next">Siguiente >></button>
                         </div>
                         <div class="btn-group">
-                            <button class="btn btn-warning" data-calendar-view="year">Año</button>
-                            <button class="btn btn-warning active" data-calendar-view="month">Mes</button>
-                            <button class="btn btn-warning" data-calendar-view="week">Semana</button>
-                            <button class="btn btn-warning" data-calendar-view="day">Dia</button>
+                            <button class="btn btn-default" data-calendar-view="year">Año</button>
+                            <button class="btn btn-default active" data-calendar-view="month">Mes</button>
+                            <button class="btn btn-default" data-calendar-view="week">Semana</button>
+                            <button class="btn btn-default" data-calendar-view="day">Dia</button>
                         </div>
                     </div>
 
@@ -53,10 +48,19 @@
         </div>
     </div>
 </div>
-<div id="eventlist"></div>
+
 <div id="subContent"></div>
 
 <script type="text/javascript">
-    loadCSS('/views/date/tmpls/card.css'); //css
-    $.getScript("./views/date/js/script.js");
+    loadCSS('./views/date/tmpls/card.css');//css card
+    loadCSS('./plugins/calendar/css/calendar.css');//css calendar
+    $.getScript("./plugins/underscore/underscore-min.js").done(function(){//js underscore
+        $.getScript("./plugins/calendar/js/calendar.js").done(function(){//js calendar
+            $.getScript("./plugins/calendar/js/es-ES.js").done(function(){//js calendar-es-ES
+
+                $.getScript("./views/date/js/script.js");
+
+            });
+        });
+    });
 </script>
